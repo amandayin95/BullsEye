85dafb5db63e9a89db2e995ac2f963e55bcde954
amandayin95 <amandayin95@gmail.com>
2016-02-01 16:35:18 -0800

    my first commit after finishing the tutorial

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Shuopeng Yin on 2/1/16.
+//  Copyright © 2016 Shuopeng Yin. All rights reserved.
+//
+
+import UIKit
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,17 +9,96 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var score = 0
+    var round = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewGame()
+        updateLabels()
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable =
+            trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func showAlert(){
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+        let title: String
+        if difference == 0 { title = "Perfect!"
+            points += 100
+        }else if difference < 5 {
+            title = "You almost had it!"
+        }else if difference < 10 {
+            title = "Pretty good!"
+        }else {
+            title = "Not even close..."
+        }
+        score += points
+        
+        let message = "You scored \(points) points"
+        let alert = UIAlertController(title: title, message: message,
+            preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "OK", style: .Default, handler: { action in
+                                                                                self.startNewRound()
+                                                                                self.updateLabels()
+                                                                                })
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
 
-
+    @IBAction func sliderMoved(slider: UISlider) {
+          currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        round += 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    @IBAction func startOver() {
+        startNewGame()
+        updateLabels()
+    }
 }
+
+
 
